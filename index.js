b955a1c59e25bb708ecc1838f25da51b4f2abaca
Hua Luo <hluo@lifesize.com>
2020-04-29 18:54:46 -0500

    add dependencies

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 (async () => {
+	const AWS = require('aws-sdk');
+    const DynamoDBWrapper = require('dynamodb-wrapper');
+    const AttributeValue = require('dynamodb-data-types').AttributeValue;
+    const fs = require('fs');
+    
 	console.log(`----- start to scan target table  -----\n`);
+
 
 	console.log(`----- target table scan finished -----\n`);
 })()

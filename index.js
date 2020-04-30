bc24cfee12e214922c3723e6ab8c6263bea7fc63
Hua Luo <hluo@lifesize.com>
2020-04-29 19:02:41 -0500

    need region

--- index.js
+++ index.js
@@ -3,9 +3,30 @@
     const DynamoDBWrapper = require('dynamodb-wrapper');
     const AttributeValue = require('dynamodb-data-types').AttributeValue;
     const fs = require('fs');
-    
-	console.log(`----- start to scan target table  -----\n`);
 
+    console.log(`----- init the connection and config-----\n`);
+    const dynamoDB = new AWS.DynamoDB({ maxRetries: 0 });
+    const dynamoDBWrapper = new DynamoDBWrapper(dynamoDB, {
+        // optionally enable DynamoDBWrapper retry logic
+        maxRetries: 6,
+        retryDelayOptions: { base: 100 }
+    });
 
-	console.log(`----- target table scan finished -----\n`);
+    const tableScanParams = {
+        TableName: 'betaCluster-IdPConfiguration',
+        ProjectionExpression: 'enterprise, idp'
+    };
+
+	console.log(`----- start to scan target table  -------\n`);
+    const allIdps = await dynamoDBWrapper.scan(tableScanParams);
+
+    let i = 1;
+    for (const wrappedItem of allIdps.Items) {
+        const idp = AttributeValue.unwrap(wrappedItem);
+        console.log(i + ":" + JSON.stringify(idp));
+        i++;
+        fs.appendFileSync('./idp_enterprise.json', idp.enterprise + "\n");
+    }
+
+	console.log(`----- target table scan finished --------\n`);
 })()
